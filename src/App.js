3547352b1ec061a6c5f90b10a33c50b94ab255e4
Joao Pedro <joaopfa@hotmail.com>
2021-04-06 21:23:14 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 function App() {
   return (
     <div>
-     
+     <h1>Filmaria</h1>
     </div>
   );
 }
---
+++ src/routes.js
@@ -0,0 +1,14 @@
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import Home from './pages/Home';
+
+const Routes = () => {
+    return(
+        <BrowserRouter>
+            <Switch>
+                <Route exact patch="/" component={Home} />
+            </Switch>
+        </BrowserRouter>
+    )
+}
+
+export default Routes;
